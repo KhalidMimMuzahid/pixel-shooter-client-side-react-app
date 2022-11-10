3aaad521515e1e0f7f1b36170edf4d227d7f91a7
Khalid Mim Muzahid <khalidmimm@gmail.com>
2022-11-10 18:36:32 +0600

    somethinge change

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -140,10 +140,7 @@ const Header = () => {
                   </Link>
                 </li>
                 <li>
-                  <Link
-                    to="/profile"
-                    className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-                  >
+                  <Link className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none">
                     <img
                       src={currentUser.photoURL}
                       alt={currentUser.displayName}
